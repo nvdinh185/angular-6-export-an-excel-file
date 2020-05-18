1cb08433485a74cbb1bfa9a8ac6ece4e897b3e04
nvdinh185 <nvdinh0766777123@gmail.com>
2020-05-18 09:08:33 +0700

    edit export

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'down-up-excel', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-    path: 'departments-kpi',
-    loadChildren: () => import('./departments-kpi/departments-kpi.module').then( m => m.DepartmentsKpiPageModule)
-  },
-  {
-    path: 'export-excel',
-    loadChildren: () => import('./export-excel/export-excel.module').then( m => m.ExportExcelPageModule)
-  },
   {
     path: 'down-up-excel',
     loadChildren: () => import('./down-up-excel/down-up-excel.module').then( m => m.DownUpExcelPageModule)
